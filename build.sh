30b70c8c9dc4a0d9ff493bd10f92b89fc034a1a8
0-Zt <salinas.ssd@gmail.com>
2024-12-14 01:52:01 -0300

    Update build configuration for Render

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
+
+# Install system dependencies
+apt-get update
+apt-get install -y wget build-essential
+
 # Install TA-Lib
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar -xvzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib/
-./configure --prefix=/usr
-make
-make install
-cd ..
-rm -rf ta-lib-0.4.0-src.tar.gz ta-lib/
+apt-get install -y ta-lib
+
 # Install Python dependencies
 pip install -r requirements.txt
